1128108f885dac08e3a0864f27fb719f517b40df
Ashish Dixit <ashishdixit815@gmail.com>
2019-12-25 22:28:58 +0530

    if user is registered with mobile number than he has to provide name and email id on .

--- app/src/main/java/com/ashish/OtpLogin.java
+++ app/src/main/java/com/ashish/OtpLogin.java
@@ -1,6 +1,7 @@
 package com.ashish;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,7 +11,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -27,6 +33,9 @@ public class OtpLogin extends AppCompatActivity {
     ProgressDialog progressDialog;
     @BindView(R.id.editText)
     EditText editText;
+    FirebaseAuth firebaseAuth;
+    String mobile="";
+
     PhoneAuthProvider.OnVerificationStateChangedCallbacks onVerificationStateChangedCallbacks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +44,11 @@ public class OtpLogin extends AppCompatActivity {
         ButterKnife.bind(this);
         progressDialog=new ProgressDialog(this);
         progressDialog.setMessage("Verifying phone number...");
+        firebaseAuth=FirebaseAuth.getInstance();
         onVerificationStateChangedCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                progressDialog.dismiss();
-                Toast.makeText(OtpLogin.this,"Verification successfull",Toast.LENGTH_SHORT).show();
-
+               signInWithMobile(phoneAuthCredential);
             }
 
             @Override
@@ -55,7 +63,7 @@ public class OtpLogin extends AppCompatActivity {
     @OnClick(R.id.btn_login_otp)
     public void onViewClicked() {
 
-        String mobile="+91"+editText.getText().toString();
+         mobile="+91"+editText.getText().toString();
         if(!mobile.equalsIgnoreCase("")){
             verifyMobile(mobile);
         }else {
@@ -65,5 +73,29 @@ public class OtpLogin extends AppCompatActivity {
     public void verifyMobile(String mobile){
         progressDialog.show();
         PhoneAuthProvider.getInstance().verifyPhoneNumber(mobile,60, TimeUnit.SECONDS,this,onVerificationStateChangedCallbacks);
+    }
+    public void signInWithMobile(PhoneAuthCredential phoneAuthCredential){
+        firebaseAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+
+                    Toast.makeText(OtpLogin.this,"Verification successfull",Toast.LENGTH_SHORT).show();
+                    FirebaseUser currentUser=task.getResult().getUser();
+                    String uid=currentUser.getUid();
+
+                    Intent intent=new Intent(OtpLogin.this,SignUp.class);
+                    intent.putExtra("MOBILE",mobile);
+                    intent.putExtra("UID",uid);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(OtpLogin.this,"Error using otp login",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/ashish/SignUp.java
+++ app/src/main/java/com/ashish/SignUp.java
@@ -2,6 +2,7 @@ package com.ashish;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ThrowOnExtraProperties;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -47,17 +49,87 @@ public class SignUp extends AppCompatActivity {
         firebaseAuth=FirebaseAuth.getInstance();
         databaseUser= FirebaseDatabase.getInstance().getReference("USERS");
         progressDialog=new ProgressDialog(this);
+        if(getIntent().hasExtra("MOBILE")){
+            signup_pass.setVisibility(View.GONE);
+        }else {
+            signup_pass.setVisibility(View.VISIBLE);
+
+        }
     }
 
     @OnClick(R.id.signup_btn)
     public void onViewClicked() {
         String name=signup_name.getText().toString();
         String email=signup_email.getText().toString();
-        String pass=signup_pass.getText().toString();
+        String pass="";
+        if(getIntent().hasExtra("MOBILE")){
+            if(!name.equalsIgnoreCase("")){
+                if(!email.equalsIgnoreCase("")){
+                    progressDialog.setMessage("Please wait...");
+                    progressDialog.show();
+                    String mobile=getIntent().getExtras().getString("MOBILE");
+                    String uid=getIntent().getExtras().getString("UID");
+                    UserInfo userInfo=new UserInfo(name,email,mobile);
+                    databaseUser.child(uid).setValue(userInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(SignUp.this,"User is registered successfully",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(SignUp.this,"Error registering user",Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this,"Please enter name",Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            pass=signup_pass.getText().toString();
+            registerUser(name,email,pass);
+        }
+
+    }
+    public void registerUser(String name,String email,String password){
         if(!name.equalsIgnoreCase("")){
             if(!email.equalsIgnoreCase("")){
-                if(!pass.equalsIgnoreCase("")){
-                    registerUser(name,email,pass);
+                if(!password.equalsIgnoreCase("")){
+                    progressDialog.setMessage("Please wait....");
+                    progressDialog.show();
+                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                FirebaseUser currentUser=firebaseAuth.getCurrentUser();
+                                String uid=currentUser.getUid();
+                                UserInfo userinfo=new UserInfo(name,email,"");
+
+
+                                databaseUser.child(uid).setValue(userinfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        progressDialog.dismiss();
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(SignUp.this,"User is registered Successfully",Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    }
+                                });
+                                Toast.makeText(SignUp.this,"user is registered",Toast.LENGTH_SHORT).show();
+                            }else {
+
+                                Toast.makeText(SignUp.this,"Error registering user\n"+name+" "+email+" "+password,Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }else {
                     Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
                 }
@@ -67,36 +139,6 @@ public class SignUp extends AppCompatActivity {
         }else {
             Toast.makeText(this,"Please enter name",Toast.LENGTH_SHORT).show();
         }
-    }
-    public void registerUser(String name,String email,String password){
-       progressDialog.setMessage("Please wait....");
-       progressDialog.show();
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                if(task.isSuccessful()){
-                    FirebaseUser currentUser=firebaseAuth.getCurrentUser();
-                    String uid=currentUser.getUid();
-                    UserInfo userinfo=new UserInfo(name,email,"");
-
-
-                    databaseUser.child(uid).setValue(userinfo).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            progressDialog.dismiss();
-                            if(task.isSuccessful()){
-                                Toast.makeText(SignUp.this,"User is registered Successfully",Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        }
-                    });
-                    Toast.makeText(SignUp.this,"user is registered",Toast.LENGTH_SHORT).show();
-                }else {
-
-                    Toast.makeText(SignUp.this,"Error registering user\n"+name+" "+email+" "+password,Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
